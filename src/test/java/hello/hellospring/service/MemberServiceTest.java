514b4526e907ced5319eb917ac3876ad002b8ef3
leebomi <bomvll@naver.com>
2021-07-23 07:57:43 +0900

    add spring bean and dependency setting

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,7 +1,10 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +12,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {
@@ -37,9 +51,8 @@ class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        //memberService.join(member2);
-        assertThrows(IllegalStateException.class, () -> memberService.join(member2)); //화살표 뒤의 로직을 실행하면, illegal예외가 터져야한다
-        //then
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));//화살표 뒤의 로직을 실행하면, illegal예외가 터져야한다
+//then
     }
     @Test
     void findMember() {
